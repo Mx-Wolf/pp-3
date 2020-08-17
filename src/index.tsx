76236ce17612fffb7aa3bc004d7f8d5dc1985b17
Maxim Volkov <mx_ruz_net@hotmail.com>
2020-08-17 17:49:50 +0300

    to be continue

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { CbrPpProps, CbrPp } from "mx-wolf-cbr-pp";
-import {StateInfo} from "./state-info";
-import {getStateInfo} from "./get-state-info"
+import { StateInfo } from "./state-info";
+import { getStateInfo } from "./get-state-info";
+import { getUserAutorization } from "./get-user-authorization";
 
 const ppInit = undefined as (Partial<CbrPpProps> | undefined);
 
@@ -10,19 +11,25 @@ const accessTokenInit = undefined as (string | undefined);
 const siInit = undefined as (StateInfo | undefined);
 export const PpContoller: React.FC = () => {
   const search = window.location.search;
+  const [err, setErr] = React.useState("");
   const [si, setSi] = React.useState(siInit);
   const [pp, setPp] = React.useState(ppInit);
   const [at, setAt] = React.useState(accessTokenInit)
   const { СуммаПрописью: sp, Сумма: s } = typeof pp === "undefined" ? { СуммаПрописью: undefined, Сумма: undefined } : pp;
 
-  React.useEffect(async () => {
+  React.useEffect(() => {
     if (typeof si === "undefined") {
       setSi(getStateInfo(search));
       return;
     }
     if (typeof at === "undefined") {
-      const {otp, xid} = si;
-      setAt(await getUserAutorization({otp,xid}))
+      const { otp, xid } = si;
+      getUserAutorization({ otp, xid })
+        .then((access_token) => setAt(access_token))
+        .catch((err) => setErr(err.message));
+      return;
+    }
+    if (err.length > 0) {
       return;
     }
     if (typeof pp === "undefined") {
@@ -32,8 +39,11 @@ export const PpContoller: React.FC = () => {
       return;
     }
     return;
-  }, [si,setSi, pp, setPp, at, setAt,search]);
+  }, [si, setSi, pp, setPp, at, setAt, search]);
 
+  if (err.length > 0) {
+    return (<div><h1>Error</h1><p>{err}</p></div>);
+  }
   if (typeof si === "undefined") {
     return (<div>...инициализация сессии</div>);
   }
